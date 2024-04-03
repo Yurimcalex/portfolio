977e05836c0cf310f66611dffb55b4f4ba60ec11
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-03 14:20:50 +0300

    Create turn page front/back function

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 //turn pages when click next or prev button
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
 
+pageTurnBtn.forEach((el, index) => {
+	el.onclick = () => {
+		const pageTurnId = el.getAttribute('data-page');
+		const pageTurn = document.getElementById(pageTurnId);
+
+		if (pageTurn.classList.contains('turn')) {
+			pageTurn.classList.remove('turn');
+			setTimeout(() => {
+				pageTurn.style.zIndex = 20 - index;
+			});
+		} else {
+			pageTurn.classList.add('turn');
+			setTimeout(() => {
+				pageTurn.style.zIndex = 20 + index;
+			});
+		}
+	};
+});
 //contact me button when click
 
 //create reverse index function
