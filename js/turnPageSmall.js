4774c96a75b86d896274c2a7afc660379a638922
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-04 20:22:01 +0300

    Create flipping page func for smaller screens

---
+++ js/turnPageSmall.js
@@ -0,0 +1,34 @@
+function smallerScreenTurnPage() {
+	const pages = document.querySelectorAll('.pg');
+	const [prevBtn, nextBtn] = document.querySelectorAll('.nextprev-btn.mediaq');
+
+	let index = 0;
+	pages[index].style.zIndex = 1;
+
+	nextBtn.onclick = function () {
+		index++;
+		if (!pages[index]) {
+			index--;
+			return;
+		}
+		pages[index].style.zIndex = 1;
+		pages[index - 1].style.zIndex = 0;
+	}
+
+	prevBtn.onclick = function () {
+		index--;
+		if (!pages[index]) {
+			index++;
+			return;
+		}
+		pages[index].style.zIndex = 1;
+		pages[index + 1].style.zIndex = 0;
+	};
+}
+
+function resetSmallZIndexes() {
+	const pages = document.querySelectorAll('.pg');
+	for (let page of pages) {
+		page.style.zIndex = 0;
+	}
+}
