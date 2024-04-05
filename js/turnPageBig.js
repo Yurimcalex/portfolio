682f51aca88dc596e06c637542b71eaef960a3ef
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-05 13:37:07 +0300

    Refact flipping page func

--- js/turnPageBig.js
+++ js/turnPageBig.js
@@ -1,105 +1,101 @@
-let animationState = {
-	enable: true
+const pages = document.querySelectorAll('.book-page.page-right');
+
+const animationState = {
+	enable: true,
+	totalPages: pages.length,
+	pageNumber: 0,
+
+	reverseIndex() {
+		this.pageNumber--;
+		if (this.pageNumber < 0) {
+			this.pageNumber = this.totalPages - 1;
+		}
+	},
+
+	toggleEnable() {
+		this.enable = !this.enable;
+	}
 };
 
-function toggleAnimation() {
-	const pages = document.querySelectorAll('.book-page.page-right');
-	animationState.enable = !animationState.enable;
 
-	if (animationState.enable) {
-		pages.forEach((page, index) => {
-			page.style.transition = '';
-		});
+function biggerScreenTurnPage() {
+	// start animation when page load
+	applyStartAnimation();
+	// turn pages when click next or prev button
+	document.querySelectorAll('.nextprev-btn').forEach((el, index) => {
+		el.onclick = () => {			
+			turnPage(el, index);
+		};
+	});
+	// contact me button when click
+	document.querySelector('.btn.contact-me').onclick = turnToContactMe;
+	// back to profile when clicked
+	document.querySelector('.back-profile').onclick = turnToProfile;
+	// toggle flipping page animation
+	document.querySelector('.toggle-animation').onclick = function () {
+		animationState.toggleEnable();
+		toggleTurnAnimation();
+		this.classList.toggle('disable');
+	};
+}
+
+
+function turnPage(page, index) {
+	const pageTurnId = page.getAttribute('data-page');
+	const pageTurn = document.getElementById(pageTurnId);
+	const flag = animationState.enable;
+
+	if (pageTurn.classList.contains('turn')) {
+		pageTurn.classList.remove('turn');
+
+		if (!flag) {
+			pageTurn.style.zIndex = 20 - index;
+		} else {
+			setTimeout(() => {
+				pageTurn.style.zIndex = 20 - index;
+			}, 500);	
+		}
+		
 	} else {
-		pages.forEach((page, index) => {
-			page.style.transition = 'unset';
-		});
+		pageTurn.classList.add('turn');
+		if (!flag) {
+			pageTurn.style.zIndex = 20 + index;
+		} else {
+			setTimeout(() => {
+				pageTurn.style.zIndex = 20 + index;
+			}, 500);
+		}
 	}
 }
 
-document.querySelector('.toggle-animation').onclick = function () {
-	toggleAnimation();
-	this.classList.toggle('disable');
-};
 
-function biggerScreenTurnPage() {
-	//turn pages when click next or prev button
-	const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
-	const pages = document.querySelectorAll('.book-page.page-right');
-	const contactMeBtn = document.querySelector('.btn.contact-me');
-	const backProfileBtn = document.querySelector('.back-profile');
-	const flag = animationState.enable;
+function turnToProfile() {
+	pages.forEach((_, index) => {
+		setTimeout(() => {
+			animationState.reverseIndex();
+			pages[animationState.pageNumber].classList.remove('turn');
 
-	pageTurnBtn.forEach((el, index) => {
-		el.onclick = () => {			
-			const pageTurnId = el.getAttribute('data-page');
-			const pageTurn = document.getElementById(pageTurnId);
-
-			if (pageTurn.classList.contains('turn')) {
-				pageTurn.classList.remove('turn');
-
-				if (!flag) {
-					pageTurn.style.zIndex = 20 - index;
-				} else {
-					setTimeout(() => {
-						pageTurn.style.zIndex = 20 - index;
-					}, 500);	
-				}
-				
-			} else {
-				pageTurn.classList.add('turn');
-				if (!flag) {
-					pageTurn.style.zIndex = 20 + index;
-				} else {
-					setTimeout(() => {
-						pageTurn.style.zIndex = 20 + index;
-					}, 500);
-				}
-			}
-		};
+			setTimeout(() => {
+				animationState.reverseIndex();
+				pages[animationState.pageNumber].style.zIndex = 10 + index;
+			}, 500);
+		}, (index + 1) * 200 + 100);
 	});
+}
 
 
+function turnToContactMe() {
+	pages.forEach((page, index) => {
+		setTimeout(() => {
+			page.classList.add('turn');
+
+			setTimeout(() => page.style.zIndex = 20 + index, 500);
+		}, (index + 1) * 200 + 100);
+	});
+}
 
 
-	//contact me button when click
-	contactMeBtn.onclick = () => {
-		pages.forEach((page, index) => {
-			setTimeout(() => {
-				page.classList.add('turn');
-
-				setTimeout(() => page.style.zIndex = 20 + index, 500);
-			}, (index + 1) * 200 + 100);
-		});
-	};
-
-	//back profile button when click
-	let totalPages = pages.length;
-	let pageNumber = 0;
-
-	function reverseIndex() {
-		pageNumber--;
-		if (pageNumber < 0) {
-			pageNumber = totalPages - 1;
-		}
-	}
-
-
-	backProfileBtn.onclick = () => {
-		pages.forEach((_, index) => {
-			setTimeout(() => {
-				reverseIndex();
-				pages[pageNumber].classList.remove('turn');
-
-				setTimeout(() => {
-					reverseIndex();
-					pages[pageNumber].style.zIndex = 10 + index;
-				}, 500);
-			}, (index + 1) * 200 + 100);
-		});
-	};
-
-	//opening animation
+function applyStartAnimation() {
 	const coverRight = document.querySelector('.cover.cover-right');
 	const pageLeft = document.querySelector('.book-page.page-left');
 
@@ -117,16 +113,30 @@ function biggerScreenTurnPage() {
 
 	pages.forEach((_, index) => {
 		setTimeout(() => {
-			reverseIndex();
-			pages[pageNumber].classList.remove('turn');
+			animationState.reverseIndex();
+			pages[animationState.pageNumber].classList.remove('turn');
 
 			setTimeout(() => {
-				reverseIndex();
-				pages[pageNumber].style.zIndex = 10 + index;
+				animationState.reverseIndex();
+				pages[animationState.pageNumber].style.zIndex = 10 + index;
 			}, 500);
 		}, (index + 1) * 200 + 2100);
 	});
 }
+
+
+function toggleTurnAnimation() {
+	if (animationState.enable) {
+		pages.forEach((page, index) => {
+			page.style.transition = '';
+		});
+	} else {
+		pages.forEach((page, index) => {
+			page.style.transition = 'unset';
+		});
+	}
+}
+
 
 function resetBigZIndexes() {
 	const pages = document.querySelectorAll('.book-page.page-right');
